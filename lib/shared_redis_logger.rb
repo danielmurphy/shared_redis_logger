9fb7a77646b572739c0ad1cd787ebfb4c4d7fd0b
Zack Hubert <zack@ministrycentered.com>
2012-09-20 16:36:44 -0700

    changing data structure

--- lib/shared_redis_logger.rb
+++ lib/shared_redis_logger.rb
@@ -20,9 +20,6 @@ module SharedRedisLogger
     def initialize
       @app_name = Rails.application.class.parent_name.downcase
       @redis = Redis.new
-      %w{debug info warn error fatal}.each do |level|
-        @redis.sadd "shared_logger:#{@app_name}:sets", level
-      end
     end
 
     # metaprograms all the .debug() etc methods
@@ -33,34 +30,13 @@ module SharedRedisLogger
       end
     end
 
-    def entries(level, start=0, per_page=50)
-      entry_list = @redis.sort("shared_logger:#{@app_name}:set:#{level}", { :limit => [ start, per_page ], :order => "DESC" })
-      fetch_entries(entry_list)
-    end
-
-    def fetch_entries(entry_list)
-      futures = []
-      @redis.multi do
-        entry_list.each do |e|
-          futures << @redis.hgetall("shared_log:#{@app_name}:#{e}")
-        end
-      end
-      entries = []
-      futures.each do |f|
-        entries << f.value
-      end
-      return entries
-    end
-
-    # when logging, we make a stored copy (for later digging) and
+    # when logging, we make a stored copy (with expiry) and
     # publish out to any listening clients
     def log_entry(message, level)
       tstamp = Time.now.to_i
-      uniq_id = "#{tstamp}#{rand(1000)}"
-      log_entry = {:message => message, :timestamp => tstamp}
-      log_entry.each { |key, value| @redis.hset "shared_log:#{@app_name}:#{uniq_id}", key, value }
-      @redis.sadd "shared_logger:#{@app_name}:set:#{level}", uniq_id
-      @redis.publish "shared_logger", log_entry.merge(:level => level, :app => @app_name).to_json
+      log_entry = {:message => message, :timestamp => tstamp, :level => level, :app => @app_name}
+      @redis.zadd "shared_logger", tstamp, log_entry
+      @redis.publish "shared_logger", log_entry.to_json
       return true
     end
   end
